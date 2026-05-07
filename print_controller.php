71a2eee51f911ca2c6ffef61cbb0636e8847ba22
Andres Pizo <pizoluligoa@gmail.com>
2026-05-06 20:24:10 -0500

    refactor: update print server endpoint to use loopback IP address and update server logs

--- print_controller.php
+++ print_controller.php
@@ -17,7 +17,7 @@ if (!$data || !isset($data['order'])) {
 }
 
 $order = $data['order'];
-$url = 'http://localhost:5000/print';
+$url = 'http://127.0.0.1:5000/print';
 
 $options = [
     'http' => [
